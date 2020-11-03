5a89764f9cbba858c10d203ecdc7429c2d5f2aee
csdingbin <bin.ding@intel.com>
2020-11-03 08:57:15 +0800

    modifed original modin+pytorch+horovod implementation

--- examples/burgerking/ScalingNotebook/BurgerKing/pytorch/modin_pytorch_burger_king.py
+++ examples/burgerking/ScalingNotebook/BurgerKing/pytorch/modin_pytorch_burger_king.py
@@ -1,18 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Burgerking workloads
+# 
+# #### using kernel: test
+# 
+# #### modin for data loading
+# #### pytorch for training
+# #### Horovod for distributed
+# #### ModinDataset was not applied
+# 
+# 
+# software version:
+# modin 0.8.1.1
+# horovod 0.20.3
+# ray 1.0.0
+
+# In[39]:
+
+
 import os
 import ray
 ray.shutdown()
-ray.init(address="10.1.0.131:6379", node_ip_address="10.1.0.131", redis_password="123") 
+#ray.init(address="10.1.0.131:6379")
+#ray.init(address='auto', _redis_password='5241590000000000')
+ray.init(address="auto")
 
 import torch 
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 import torch.nn.functional as F
-import boto3
+#import boto3
+import pandas
 import modin.pandas as pd
 
 import numpy as np
-import s3fs
+#import s3fs
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 import logging
@@ -25,12 +49,25 @@ from datetime import datetime
 import horovod.torch as hvd
 
 
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-logging.basicConfig(filename='./drivethru_log',level=logging.DEBUG)
+# In[ ]:
+
+
+
+
+
+# In[17]:
+
+
+hvd.init()
+
+
+# In[10]:
+
+
+
 
 bucket = ...
-prefix = '/home/sparkuser/jupyter/Bin/BlueWhale-POC/data/'
+prefix = '/mnt/DP_disk7/data_10000/'
 model_prefix = ...
 batch_size = 20
 num_epoch = 1
@@ -45,9 +82,13 @@ df_list = []
 num_files=10
 for num in range(0,1):
     df = pd.read_json(prefix+str(num)+".json", orient='columns', lines=True)
+    #df = pd.DataFrame(pdf)
     df_list.append(df)
 
 data = pd.concat(df_list)
+
+
+# In[31]:
 
 
 n_plus=522
@@ -80,13 +121,20 @@ feelsBucket_tensor=torch.LongTensor(feelsBucket.values)
 target_tensor=torch.LongTensor(target.values)
 
 Train_Dataset=TensorDataset(pluids_tensor,bkidx_tensor,timeidx_tensor,weatheridx_tensor,feelsBucket_tensor,target_tensor)
-
+train_sampler = torch.utils.data.distributed.DistributedSampler(
+        Train_Dataset, num_replicas=hvd.size(), rank=hvd.rank())
 train_loader = torch.utils.data.DataLoader(dataset=Train_Dataset,
-                                           batch_size=batch_size,shuffle=True)
+                                           batch_size=batch_size, sampler=train_sampler)
+
+
 time2=datetime.now()
 data_prepare_time=time2-time1
 print("data load time:")
 print(data_prepare_time)
+
+
+# In[32]:
+
 
 def plu_embedding(pluids):
     embeds_pluids = nn.Embedding(n_plus, 50)
@@ -120,6 +168,9 @@ def weather_embedding(weather):
     return weather_embed
 
 
+# In[33]:
+
+
 # Bidirectional recurrent neural network (many-to-one)
 class BiRNN(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers,fcn_input_size,fcn_output_size):
@@ -127,6 +178,11 @@ class BiRNN(nn.Module):
         self.hidden_size = hidden_size
         self.num_layers = num_layers
         
+        self.embeds_pluids = nn.Embedding(n_plus, 50)
+        self.embeds_bkidx = nn.Embedding(n_bkids, 100)
+        self.embeds_timeidx = nn.Embedding(n_time, 100)
+        self.embeds_feelsBucket = nn.Embedding(n_feels, 100)
+        self.embeds_weather = nn.Embedding(n_weather, 100)
         
         
         self.gru = nn.GRU(input_size, hidden_size, num_layers, batch_first=True, bidirectional=True)
@@ -145,11 +201,11 @@ class BiRNN(nn.Module):
     def forward(self,pluids,bkidx,timeidx,weatheridx,feelsBucket):
         # Set initial states
         
-        plu_embed=plu_embedding(pluids)
-        bkidx_embed=bkidx_embedding(bkidx)
-        time_embed=timeidx_embedding(timeidx)
-        weather_embed=weather_embedding(weatheridx)
-        feels_embed=feels_embedding(feelsBucket)
+        plu_embed = self.embeds_pluids(pluids.type(torch.LongTensor)).squeeze()
+        bkidx_embed = self.embeds_bkidx(bkidx.type(torch.LongTensor)).squeeze()
+        time_embed = self.embeds_timeidx(timeidx.type(torch.LongTensor)).squeeze()
+        weather_embed = self.embeds_weather(weatheridx.type(torch.LongTensor)).squeeze()
+        feels_embed = self.embeds_feelsBucket(feelsBucket.type(torch.LongTensor)).squeeze()
 
         x=plu_embed
         h0 = torch.zeros(self.num_layers*2, x.size(0), self.hidden_size) # 2 for bidirection 
@@ -169,28 +225,36 @@ class BiRNN(nn.Module):
         
         #flatten the output
         attention_output =self.flatten(output)
-        
         context_features=torch.mul(attention_output,(1 + bkidx_embed + time_embed + weather_embed + feels_embed))
-        
-
         ac1=F.relu(context_features)
         dropout1=self.drop_layer(ac1)
         output=self.fc(dropout1)
+       
+        #output=F.softmax(fc1)
         return output
 
-optimizer = torch.optim.Adagrad(model.parameters(), lr=learning_rate)
 
-optimizer= hvd.DistributedOptimizer(optimizer,
-                                         named_parameters=model.named_parameters(),
-                                         compression=compression)
-hvd.broadcast_parameters(model.state_dict(), root_rank=0)
-hvd.broadcast_optimizer_state(optimizer, root_rank=0)
+# In[34]:
 
 
 model=BiRNN(50, 50, 1,100,522).to(device)
 learning_rate=0.01
 
 criterion = nn.CrossEntropyLoss()
+
+
+# In[35]:
+
+
+optimizer = torch.optim.Adagrad(model.parameters(), lr=learning_rate)
+
+optimizer= hvd.DistributedOptimizer(optimizer,
+                                         named_parameters=model.named_parameters())
+hvd.broadcast_parameters(model.state_dict(), root_rank=0)
+hvd.broadcast_optimizer_state(optimizer, root_rank=0)
+
+
+# In[36]:
 
 
 time3=datetime.now()
@@ -201,7 +265,7 @@ for epoch in range(num_epoch):
            
        
         output=model(pluids,bkidx,timeidx,weatheridx,feelsBucket)
-        label=Variable(target)
+        label=Variable(target).squeeze()
         
         loss = criterion(output, label)
         
@@ -216,6 +280,10 @@ time4=datetime.now()
 model_train_time=time4-time3
 print("model train time")
 print(model_train_time)
+
+
+# In[37]:
+
 
 ## Evaluation process
 
@@ -254,6 +322,9 @@ test_weather_embed=weather_embedding(test_weatheridx_tensor)
 test_feels_embed=feels_embedding(test_feelsBucket_tensor)
 
 
+# In[38]:
+
+
 test_Dataset=TensorDataset(test_pluids_tensor,test_bkidx_tensor,test_timeidx_tensor,test_weatheridx_tensor,test_feelsBucket_tensor,test_target_tensor)
 test_loader = torch.utils.data.DataLoader(dataset=test_Dataset,
                                            batch_size=batch_size,shuffle=True)
@@ -270,3 +341,10 @@ total=len(test_target)
 
 print("Accuracy:%f"%(correct/total).cpu().detach().data.numpy())
 print(model_train_time)
+
+
+# In[ ]:
+
+
+
+
