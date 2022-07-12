afd925b82193e5fbd06f7fcb0145447540da6517
csdingbin <bin.ding@intel.com>
2022-07-12 14:50:08 +0800

    add NTK function to basic utils (#168)
    * add NTK function to basic utils
    
    * refine

--- DeNas/scores/basic_utils.py
+++ DeNas/scores/basic_utils.py
@@ -86,8 +86,52 @@ def compute_synflow_per_weight(model_type, net, inputs, mode):
 
     return grads_abs
 
+def get_ntk_n(networks, recalbn=0, train_mode=False, num_batch=None,
+              batch_size=None, image_size=None, gpu=None):
+    if gpu is not None:
+        device = torch.device('cuda:{}'.format(gpu))
+    else:
+        device = torch.device('cpu')
 
 
+    ntks = []
+    for network in networks:
+        if train_mode:
+            network.train()
+        else:
+            network.eval()
+    grads = [[] for _ in range(len(networks))]
 
+    for i in range(num_batch):
+        inputs = torch.randn((batch_size, 3, image_size, image_size), device=device)
+        for net_idx, network in enumerate(networks):
+            network.zero_grad()
+            if gpu is not None:
+                inputs_ = inputs.clone().cuda(device=device, non_blocking=True)
+            else:
+                inputs_ = inputs.clone()
+
+            logit = network(inputs_)
+            if isinstance(logit, tuple):
+                logit = logit[1]  
+            for _idx in range(len(inputs_)):
+                logit[_idx:_idx+1].backward(torch.ones_like(logit[_idx:_idx+1]), retain_graph=True)
+                grad = []
+                for name, W in network.named_parameters():
+                    if 'weight' in name and W.grad is not None:
+                        grad.append(W.grad.view(-1).detach())
+                grads[net_idx].append(torch.cat(grad, -1))
+                network.zero_grad()
+                if gpu is not None:
+                    torch.cuda.empty_cache()
+
+    ######
+    grads = [torch.stack(_grads, 0) for _grads in grads]
+    ntks = [torch.einsum('nc,mc->nm', [_grads, _grads]) for _grads in grads]
+    conds = []
+    for ntk in ntks:
+        eigenvalues, _ = torch.symeig(ntk)  # ascending
+        conds.append(np.nan_to_num((eigenvalues[-1] / eigenvalues[0]).item(), copy=True))
+    return conds
 
 
