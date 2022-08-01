8bbeaa2568a9449e59e584dae19af73d0d781eb7
Hua, Xiaozhuan <xiaozhuan.hua@intel.com>
2022-08-01 14:10:21 +0800

    transfer learning: bugfix, ComposedDataset length bug

--- AIDK/TransferLearningKit/src/dataset/composed_dataset.py
+++ AIDK/TransferLearningKit/src/dataset/composed_dataset.py
@@ -16,13 +16,23 @@ class ComposedDataset(Dataset):
         :param datasets: a series of datasets
         '''
         self._datasets = datasets
+
+        ############ len list ############
         self._len_list = []
         for dataset in datasets:
             dataset_len = len(dataset)
             self._len_list.append(dataset_len)
             logging.info('dataset [%s] len = %s'%(str(dataset),dataset_len))
+        ############ max len #############
+        self._max_len = self._len_list[0]
+        for _len in self._len_list:  # get the max len
+            if _len > self._max_len:
+                self._max_len = _len
 
     def __getitem__(self, index):
+        if index >= self._max_len: # out range
+            raise StopIteration
+
         data_list = []
         label_list = []
         for (dataset,_len) in zip(self._datasets,self._len_list):
@@ -33,11 +43,7 @@ class ComposedDataset(Dataset):
         return (data_list,label_list)
 
     def __len__(self):
-        max_len = self._len_list[0]
-        for _len in self._len_list: # get the max len
-            if _len > max_len:
-                max_len = _len
-        return max_len
+        return self._max_len
 
     def __str__(self):
         output = 'ComposedDataset: length [%s]\n'% self.__len__()
