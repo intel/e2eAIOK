3af06640d711e9ecf50a92c58a5799931317a1b7
Zheng Cai <zheng.cai@intel.com>
2022-08-31 18:54:00 +0800

    refine torchccl

--- tests/cicd/docker_torchccl_test.sh
+++ tests/cicd/docker_torchccl_test.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# enable oneAPI
-source /opt/intel/oneapi/setvars.sh --ccl-configuration=cpu_icc --force
+# init conda
+eval "$('/opt/intel/oneapi/intelpython/latest/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+
 # UT check for docker env with torchccl
+source /opt/intel/oneapi/intelpython/latest/envs/pytorch_mlperf/.local/env/setvars.sh
 cd /home/vmagent/app/e2eaiok
-conda activate pytorch_1.10
+conda activate pytorch_mlperf
 mpirun -n 2 -l pytest -v --capture=tee-sys tests/cicd/src/test_docker_torchccl.py
