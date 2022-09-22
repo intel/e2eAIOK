a0b3ca476ce3148c4af8504dea2d029cd729c4ef
Tao He <tao1.he@intel.com>
2022-09-22 16:30:52 +0800

    asr add sigopt search engine (#242)

--- DeNas/search/SigoptSearchEngine.py
+++ DeNas/search/SigoptSearchEngine.py
@@ -192,6 +192,72 @@ class SigoptSearchEngine(BaseSearchEngine):
                 cand_tuple.append(int(best_assignments[num_heads_name]))
             cand_tuple.append(int(best_assignments['EMBED_DIM']))
             self.best_struct = tuple(cand_tuple)
+        elif self.params.domain == "asr":
+            experiment = self.conn.experiments().create(
+                name= 'asr denas',
+                project='denas',
+                type="offline",
+                observation_budget=1000,
+                metrics=[dict(name='DeScore', objective='maximize')],
+                conditionals=[dict(name="DEPTH",values=["9","10","11","12"])],
+                parameters=[
+                    dict(name="MLP_RATIO_0", type="double", grid=[*self.search_space['mlp_ratio']]),
+                    dict(name="MLP_RATIO_1", type="double", grid=[*self.search_space['mlp_ratio']]),
+                    dict(name="MLP_RATIO_2", type="double", grid=[*self.search_space['mlp_ratio']]),
+                    dict(name="MLP_RATIO_3", type="double", grid=[*self.search_space['mlp_ratio']]),
+                    dict(name="MLP_RATIO_4", type="double", grid=[*self.search_space['mlp_ratio']]),
+                    dict(name="MLP_RATIO_5", type="double", grid=[*self.search_space['mlp_ratio']]),
+                    dict(name="MLP_RATIO_6", type="double", grid=[*self.search_space['mlp_ratio']]),
+                    dict(name="MLP_RATIO_7", type="double", grid=[*self.search_space['mlp_ratio']]),
+                    dict(name="MLP_RATIO_8", type="double", grid=[*self.search_space['mlp_ratio']]),
+                    dict(name="MLP_RATIO_9", type="double", grid=[*self.search_space['mlp_ratio']], conditions=dict(DEPTH=["12","11","10"])),
+                    dict(name="MLP_RATIO_10", type="double", grid=[*self.search_space['mlp_ratio']], conditions=dict(DEPTH=["12","11"])),
+                    dict(name="MLP_RATIO_11", type="double", grid=[*self.search_space['mlp_ratio']], conditions=dict(DEPTH=["12"])),
+                    dict(name="NUM_HEADS_0", type="int", grid=[*self.search_space['num_heads']]),
+                    dict(name="NUM_HEADS_1", type="int", grid=[*self.search_space['num_heads']]),
+                    dict(name="NUM_HEADS_2", type="int", grid=[*self.search_space['num_heads']]),
+                    dict(name="NUM_HEADS_3", type="int", grid=[*self.search_space['num_heads']]),
+                    dict(name="NUM_HEADS_4", type="int", grid=[*self.search_space['num_heads']]),
+                    dict(name="NUM_HEADS_5", type="int", grid=[*self.search_space['num_heads']]),
+                    dict(name="NUM_HEADS_6", type="int", grid=[*self.search_space['num_heads']]),
+                    dict(name="NUM_HEADS_7", type="int", grid=[*self.search_space['num_heads']]),
+                    dict(name="NUM_HEADS_8", type="int", grid=[*self.search_space['num_heads']]),
+                    dict(name="NUM_HEADS_9", type="int", grid=[*self.search_space['num_heads']], conditions=dict(DEPTH=["12","11","10"])),
+                    dict(name="NUM_HEADS_10", type="int", grid=[*self.search_space['num_heads']], conditions=dict(DEPTH=["12","11"])),
+                    dict(name="NUM_HEADS_11", type="int", grid=[*self.search_space['num_heads']], conditions=dict(DEPTH=["12"])),
+                    dict(name="EMBED_DIM", type="int", grid=[*self.search_space['embed_dim']]),
+                ],
+            )
+            for epoch in range(experiment.observation_budget):
+                suggestion = self._get_sigopt_suggestion(experiment)
+                cand_tuple = list()
+                depth = int(suggestion.assignments['DEPTH'])
+                cand_tuple.append(depth)
+                for i in range(depth):
+                    mlp_ratio_name = f"MLP_RATIO_{i}"
+                    cand_tuple.append(float(suggestion.assignments[mlp_ratio_name]))
+                for i in range(depth):
+                    num_heads_name = f"NUM_HEADS_{i}"
+                    cand_tuple.append(int(suggestion.assignments[num_heads_name]))
+                cand_tuple.append(int(suggestion.assignments['EMBED_DIM']))
+                cand = tuple(cand_tuple)
+                if not self.cand_islegal(cand):
+                    self._set_illegal_observation(experiment, suggestion.id)
+                    continue
+                nas_score = self.cand_evaluate(cand).item()
+                self.logger.info('epoch = {} nas_score = {} cand = {}'.format(epoch, nas_score, cand))
+                self._set_sigopt_observation(experiment, suggestion.id, nas_score)
+            best_assignments = self.conn.experiments(experiment.id).best_assignments().fetch().data[0].assignments  
+            depth = int(best_assignments['DEPTH'])
+            cand_tuple.append(depth)
+            for i in range(depth):
+                mlp_ratio_name = f"MLP_RATIO_{i}"
+                cand_tuple.append(float(best_assignments[mlp_ratio_name]))
+            for i in range(depth):
+                num_heads_name = f"NUM_HEADS_{i}"
+                cand_tuple.append(int(best_assignments[num_heads_name]))
+            cand_tuple.append(int(best_assignments['EMBED_DIM']))
+            self.best_struct = tuple(cand_tuple)
         else:
             raise RuntimeError(f"Domain {self.params.domain} is not supported")
 
