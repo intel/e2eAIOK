a92168320fd044cf22272f62289b23d74363a714
Zheng Cai <zheng.cai@intel.com>
2022-11-29 11:27:39 +0800

    minor fix about setup and module init

--- setup.py
+++ setup.py
@@ -1,37 +1,37 @@
 import os
-import setuptools
-from setuptools.command.install import install
-from setuptools import find_packages
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 e2eaiok_home = os.path.abspath(__file__ + "/../")
-try:
-    VERSION = open(os.path.join(e2eaiok_home, 'e2eAIOK/', 'version'), 'r').read().strip()
-except:
-    VERSION = '0.2.1'
+VERSION = open(os.path.join(e2eaiok_home, 'e2eAIOK/', 'version'), 'r').read().strip()
 
-setuptools.setup(
-    name="e2eAIOK",
-    version=VERSION,
-    author="INTEL AIA BDF",
-    author_email="chendi.xue@intel.com",
-    description=
-    "Intel® End-to-End AI Optimization Kit",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/intel/e2eAIOK",
-    project_urls={
-        "Bug Tracker": "https://github.com/intel/e2eAIOK/issues",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-    ],
-    packages=find_packages(exclude=["RecDP", "modelzoo", "example"]),
-    package_data = {'e2eAIOK': ['version']},
-    python_requires=">=3.6",
-    zip_safe=False,
-    install_requires=[])
+def setup_package():
+    metadata = dict(
+        name="e2eAIOK",
+        version=VERSION,
+        author="INTEL AIA BDF",
+        author_email="chendi.xue@intel.com",
+        description="Intel® End-to-End AI Optimization Kit",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/intel/e2eAIOK",
+        project_urls={
+            "Bug Tracker": "https://github.com/intel/e2eAIOK/issues",
+        },
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: OS Independent",
+        ],
+        packages=find_packages(exclude=["RecDP", "modelzoo", "example"]),
+        package_data = {'e2eAIOK': ['version']},
+        python_requires=">=3.6",
+        zip_safe=False,
+        install_requires=[]
+    )
+    setup(**metadata)
+
+if __name__ == '__main__':
+    setup_package()
