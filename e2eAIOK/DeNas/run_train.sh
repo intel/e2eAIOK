2e6ae48f1ddd66cc74ae2548cd1ea133d7015fed
Bin Ding <bin.ding@intel.com>
2022-11-30 08:30:12 +0800

    trigger

--- e2eAIOK/DeNas/run_train.sh
+++ e2eAIOK/DeNas/run_train.sh
@@ -5,7 +5,7 @@ RANDOM_SEED=`date +%s`
 # python -m intel_extension_for_pytorch.cpu.launch --distributed --nproc_per_node=2 --nnodes=1 /home/vmagent/app/e2eaiok/e2eAIOK/DeNas/train.py \
 #     --domain asr --conf /home/vmagent/app/e2eaiok/conf/denas/asr/e2eaiok_denas_train_asr.conf --random_seed 74443 2>&1 | tee ASR_training_${RANDOM_SEED}.log
 
-#CNN Single process traing
+#CNN Single node two process traing
 # python -m intel_extension_for_pytorch.cpu.launch --distributed --nproc_per_node=2 --nnodes=1  /home/vmagent/app/e2eaiok/e2eAIOK/DeNas/train.py \
 #      --domain cnn --conf /home/vmagent/app/e2eaiok/conf/denas/cv/e2eaiok_denas_train_cnn.conf 2>&1 | tee CNN_training_${RANDOM_SEED}.log
 
