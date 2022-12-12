d15418a8e9449f2e05dc2cbb797262db7397edf7
Zheng Cai <zheng.cai@intel.com>
2022-12-12 13:43:26 +0800

    initial travis unittest framework

--- tests/unittest/test_hydrodb.py
+++ tests/unittest/test_hydrodb.py
@@ -2,15 +2,13 @@ from e2eAIOK.utils.hydroDB import *
 from e2eAIOK.utils.hydromodel import *
 from e2eAIOK.utils.hydroconfig import *
 
-settings = init_settings()
-hydro_model = HydroModel(settings)
-db = HydroDB()
+class TestHydroDB:
+    settings = init_settings()
+    hydro_model = HydroModel(settings)
+    db = HydroDB(hydrodb_path='')
 
-out_hydro_model = db.get_model_by_id('dummy_id')
-if out_hydro_model:
-    print("This test has been ran before, to run full test, please delete e2eaiok.db")
-
-db.insert_model_by_id('dummy_id', hydro_model)
-out_hydro_model = db.get_model_by_id('dummy_id')
-assert(out_hydro_model != None)
-assert(out_hydro_model.to_json() == hydro_model.to_json())
+    def test_hydrodb(self):
+        self.db.insert_model_by_id('dummy_id', self.hydro_model)
+        out_hydro_model = self.db.get_model_by_id('dummy_id')
+        assert(out_hydro_model != None)
+        assert(out_hydro_model.to_json() == self.hydro_model.to_json())
