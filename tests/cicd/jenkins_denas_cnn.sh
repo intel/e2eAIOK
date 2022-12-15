fd7767e127ee2132f543b7d7b43e3889daa3ae22
Zheng Cai <zheng.cai@intel.com>
2022-12-15 16:43:18 +0800

    add self-hosted ci workflow and add ci for denas train

--- tests/cicd/jenkins_denas_cnn.sh
+++ tests/cicd/jenkins_denas_cnn.sh
@@ -4,6 +4,10 @@
 eval "$('/opt/intel/oneapi/intelpython/latest/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 conda activate pytorch
 
+# reinstall e2eaiok from source
+cd /home/vmagent/app/e2eaiok
+python setup.py sdist && pip install dist/e2eAIOK-*.*.*.tar.gz
+
 # launch denas for cnn-based supernet
 cd /home/vmagent/app/e2eaiok/e2eAIOK/DeNas
 sed -i '/max_epochs:/ s/:.*/: 1/' ../../conf/denas/cv/e2eaiok_denas_cnn.conf
@@ -12,3 +16,7 @@ cd /home/vmagent/app/e2eaiok
 
 # test
 LANG=C tests/cicd/bats/bin/bats tests/cicd/test_denas.bats
+
+# train
+cd /home/vmagent/app/e2eaiok/e2eAIOK/DeNas
+python -m intel_extension_for_pytorch.cpu.launch --distributed --nproc_per_node=2 --nnodes=1 train.py --domain cnn --conf /home/vmagent/app/e2eaiok/conf/denas/cv/e2eaiok_denas_train_cnn.conf
