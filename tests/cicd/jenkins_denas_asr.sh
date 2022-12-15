fa5cf7073406ac90aa875df8e81e9e6546d43341
Zheng Cai <zheng.cai@intel.com>
2022-12-15 09:51:30 +0800

    fix denas cicd

--- tests/cicd/jenkins_denas_asr.sh
+++ tests/cicd/jenkins_denas_asr.sh
@@ -4,6 +4,9 @@
 eval "$('/opt/intel/oneapi/intelpython/latest/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 conda activate pytorch
 
+# reinstall e2eaiok from source
+cd /home/vmagent/app/e2eaiok
+python setup.py sdist && pip install dist/e2eAIOK-*.*.*.tar.gz
 # launch denas for transformer-based asr
 cd /home/vmagent/app/e2eaiok/e2eAIOK/DeNas
 sed -i '/max_epochs:/ s/:.*/: 1/' ../../conf/denas/asr/e2eaiok_denas_asr.conf
