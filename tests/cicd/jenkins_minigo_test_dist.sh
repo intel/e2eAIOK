b1da656d38905c89ee6c81a54dd7e7f8c40f740a
Zheng Cai <zheng.cai@intel.com>
2022-12-16 22:55:33 +0800

    refine

--- tests/cicd/jenkins_minigo_test_dist.sh
+++ tests/cicd/jenkins_minigo_test_dist.sh
@@ -21,7 +21,8 @@ set -e
 cd modelzoo/minigo
 conda activate minigo
 printf "\n" | ./cc/configure_tensorflow.sh
-HOME=/root ./ml_perf/scripts/cc_libgen_parallel_selfplay.sh
+export HOME=/root
+./ml_perf/scripts/cc_libgen_parallel_selfplay.sh
 # make MiniGo CI/CD test process faster
 sed -i '/--winrate=/ s/=.*/=0.003/' ml_perf/flags/19/train_loop.flags
 sed -i '/--eval=/ s/=.*/=1/' ml_perf/flags/19/train_loop.flags
--- tests/cicd/jenkins_minigo_test_prepare.sh
+++ tests/cicd/jenkins_minigo_test_prepare.sh
@@ -4,5 +4,6 @@ eval "$('/root/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 cd modelzoo/minigo
 conda activate minigo
 printf "\n" | ./cc/configure_tensorflow.sh
-HOME=/root ./ml_perf/scripts/cc_libgen_parallel_selfplay.sh
+export HOME=/root
+./ml_perf/scripts/cc_libgen_parallel_selfplay.sh
 cd ../..
