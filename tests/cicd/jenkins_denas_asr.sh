31c8bb645de71e66cd0e4b0a2b6e6dfa55ac83c1
Zheng Cai <zheng.cai@intel.com>
2022-12-19 11:23:44 +0800

    revert unchanged files

--- tests/cicd/jenkins_denas_asr.sh
+++ tests/cicd/jenkins_denas_asr.sh
@@ -7,6 +7,7 @@ conda activate pytorch
 # reinstall e2eaiok from source
 cd /home/vmagent/app/e2eaiok
 python setup.py sdist && pip install dist/e2eAIOK-*.*.*.tar.gz
+
 # launch denas for transformer-based asr
 cd /home/vmagent/app/e2eaiok/e2eAIOK/DeNas
 sed -i '/max_epochs:/ s/:.*/: 1/' ../../conf/denas/asr/e2eaiok_denas_asr.conf
--- tests/cicd/jenkins_denas_bert.sh
+++ tests/cicd/jenkins_denas_bert.sh
@@ -7,6 +7,7 @@ conda activate pytorch
 # reinstall e2eaiok from source
 cd /home/vmagent/app/e2eaiok
 python setup.py sdist && pip install dist/e2eAIOK-*.*.*.tar.gz
+
 # launch denas for bert-based supernet
 cd /home/vmagent/app/e2eaiok/e2eAIOK/DeNas
 sed -i '/max_epochs:/ s/:.*/: 1/' ../../conf/denas/nlp/e2eaiok_denas_bert.conf
