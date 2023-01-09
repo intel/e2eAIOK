b9689ce4ea80ca8ffb0b4000a9caabc9cbf29924
Zheng Cai <zheng.cai@intel.com>
2023-01-09 16:38:42 +0800

    refine

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 import os
 import sys
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+try:
+    # pip >=20
+    from pip._internal.network.session import PipSession
+    from pip._internal.req import parse_requirements
+except ImportError:
+    try:
+        # 10.0.0 <= pip <= 19.3.1
+        from pip._internal.download import PipSession
+        from pip._internal.req import parse_requirements
+    except ImportError:
+        # pip <= 9.0.3
+        from pip.download import PipSession
+        from pip.req import parse_requirements
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
