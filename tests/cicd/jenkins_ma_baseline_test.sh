31599616a0a19c4bcabe389b6a55b0b10be95a67
Xinyao Wang <xinyao.wang@intel.com>
2023-01-17 10:45:17 +0800

    remove extra workflow

--- tests/cicd/jenkins_ma_baseline_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# init conda env
-eval "$('/opt/intel/oneapi/intelpython/latest/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-# activate conda
-conda activate pytorch-1.12.0
-# run main.py
-python /home/vmagent/app/e2eaiok/e2eAIOK/ModelAdapter/src/main.py --cfg /home/vmagent/app/e2eaiok/conf/ma/demo/baseline/cifar100_res18.yaml
--- tests/cicd/jenkins_ma_finetuner_autotune_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# init conda env
-eval "$('/opt/intel/oneapi/intelpython/latest/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-# activate conda
-conda activate pytorch-1.12.0
-# run main.py
-python /home/vmagent/app/e2eaiok/e2eAIOK/ModelAdapter/src/main.py -R1 --cfg /home/vmagent/app/e2eaiok/conf/ma/demo/finetuner/cifar100_res50PretrainI21k.yaml
