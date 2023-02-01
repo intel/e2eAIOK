06bd319194f62bdf6d9dc44c7c611c74bdd25080
tianyil1 <tianyi.liu@intel.com>
2023-02-01 15:07:40 +0800

    Add use-defined search space features

--- e2eAIOK/DeNas/thirdparty/supernet_hf.py
+++ e2eAIOK/DeNas/thirdparty/supernet_hf.py
@@ -35,9 +35,16 @@ class SuperHFModel(AutoModel):
     def search_space_generation(cls, pretrained_model_name_or_path, **kwargs):
         hf_config = AutoConfig.from_pretrained(pretrained_model_name_or_path)
         search_space = {}
-        search_space["num_hidden_layers"] = range(int(hf_config.num_hidden_layers/2), int(hf_config.num_hidden_layers), 1)
-        search_space["num_attention_heads"] = range(int(hf_config.num_attention_heads/2), int(hf_config.num_attention_heads), 1)
-        search_space["hidden_size"] = range(int(hf_config.hidden_size/2), int(hf_config.hidden_size), 16)
+        search_space["num_hidden_layers"] = list(range(int(hf_config.num_hidden_layers/2), int(hf_config.num_hidden_layers), 1))
+        search_space["num_attention_heads"] = list(range(int(hf_config.num_attention_heads/2), int(hf_config.num_attention_heads), 1))
+        search_space["hidden_size"] = list(range(int(hf_config.hidden_size/2), int(hf_config.hidden_size), 16))
         for k in kwargs:
-            search_space[k] = range(int(kwargs[k]/2), int(kwargs[k]), 16)
+            if 'max' not in kwargs[k]:
+                raise ValueError("Please specify the up bound of {} in search space".format(k))
+            search_range = {"min": int(kwargs[k]['max']/2), "max": int(kwargs[k]['max']), "step": 1}
+            if 'min' in kwargs[k]:
+                search_range["min"] = int(kwargs[k]["min"])
+            if 'step' in kwargs[k]:
+                search_range["step"] = int(kwargs[k]["step"])
+            search_space[k] = list(range(search_range["min"], search_range["max"], search_range["step"]))
         return search_space
