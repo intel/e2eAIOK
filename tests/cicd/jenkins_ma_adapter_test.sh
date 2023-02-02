9eca22a1cf7dd75e3aa8f091979a574854fa9128
zhouyu5 <yu.zhou@intel.com>
2023-02-02 09:58:06 +0800

    add cicd scripts

---
+++ tests/cicd/jenkins_ma_adapter_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# init conda env
+eval "$('/opt/intel/oneapi/intelpython/latest/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+# activate conda
+conda activate pytorch-1.12.0
+cd /home/vmagent/app/e2eaiok
+python setup.py sdist && pip install dist/e2eAIOK-*.*.*.tar.gz
+# run main.py
+cd /home/vmagent/app/e2eaiok/modelzoo/unet
+sh patch_unet.sh
+sh scripts/run_single_demo.sh
