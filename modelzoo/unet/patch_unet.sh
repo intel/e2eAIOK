6b16c2d358d309f4840ae71831bad8ced3f5b708
zhouyu5 <yu.zhou@intel.com>
2023-02-20 11:42:08 +0800

    diff patch -> git patch

--- modelzoo/unet/patch_unet.sh
+++ modelzoo/unet/patch_unet.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # generate patch
-# cp -r ../third_party/nnUNet/ .
-# diff -urN nnUNet/ nnUNetNew > nnunet.patch
+# git diff --cached > git.patch
 
 get_original_model () {
     cp -r ../third_party/nnUNet .
 }
 
 apply_patch () {
-    patch -p0 < nnunet.patch
     cd nnUNet
+    git apply ../git.patch
     pip install -e .
+    cd ..
 }
 
 get_original_model
