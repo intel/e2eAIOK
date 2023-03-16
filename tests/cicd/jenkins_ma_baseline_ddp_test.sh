a45741fe872ce17a1929c27d50fd1e4c2f04db9a
Yu-Zhou <yu.zhou@intel.com>
2023-03-17 03:28:35 +0800

    [WIP][v1.1][ISSUE-209] update oneapi version (#210)
    * update oneapi version
    
    * update ipex versionn
    
    * fix env name
    
    * fix torch vision

--- tests/cicd/jenkins_ma_baseline_ddp_test.sh
+++ tests/cicd/jenkins_ma_baseline_ddp_test.sh
@@ -3,7 +3,7 @@
 # init conda env
 eval "$('/opt/intel/oneapi/intelpython/latest/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 # activate conda
-conda activate pytorch-1.12.0
+conda activate pytorch
 cd /home/vmagent/app/e2eaiok
 python setup.py sdist && pip install dist/e2eAIOK-*.*.*.tar.gz
 # run main.py
