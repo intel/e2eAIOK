1c0a5e8727d365504b87cd246f4a5d63061738c1
Zheng Cai <zheng.cai@intel.com>
2023-04-03 17:45:23 +0800

    add jupyter_mode support

--- Dockerfile-ubuntu/notebook/start-notebook.sh
+++ Dockerfile-ubuntu/notebook/start-notebook.sh
@@ -2,4 +2,4 @@
 port=${port:-12888}
 token=${token:-""}
 
-nohup jupyter-lab --notebook-dir=./ --ip=0.0.0.0 --port=$port --no-browser --NotebookApp.token=$token --allow-root --no-browser & disown
+jupyter-lab --notebook-dir=./ --ip=0.0.0.0 --port=$port --no-browser --NotebookApp.token=$token --allow-root
