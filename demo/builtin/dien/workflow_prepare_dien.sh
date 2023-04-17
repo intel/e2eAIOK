98830af7c5249f7bae12b51afd8f41092ce99c76
Xue, Chendi <chendi.xue@intel.com>
2023-04-17 19:01:31 +0000

    update dien demo
    Signed-off-by: Xue, Chendi <chendi.xue@intel.com>

--- demo/builtin/dien/workflow_prepare_dien.sh
+++ demo/builtin/dien/workflow_prepare_dien.sh
@@ -1,3 +1,4 @@
-git clone https://github.com/intel/e2eAIOK.git; cd e2eAIOK; git submodule update --init --recursive
+git clone https://github.com/intel/e2eAIOK.git;
 mv e2eAIOK e2eaiok
-cd e2eaiok/modelzoo/dien/train; sh patch_dien.sh
+cd e2eaiok; git submodule update --init --recursive
+cd modelzoo/dien/train; sh patch_dien.sh
