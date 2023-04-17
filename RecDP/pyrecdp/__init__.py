d9bc2b105f9ccada61deb907ffb25c9aac470da1
Xue, Chendi <chendi.xue@intel.com>
2023-04-17 22:43:51 +0000

    fix JAVA_HOME issue when user specify JAVA_HOME already
    Signed-off-by: Xue, Chendi <chendi.xue@intel.com>

--- RecDP/pyrecdp/__init__.py
+++ RecDP/pyrecdp/__init__.py
@@ -5,7 +5,10 @@ try:
 except:
     raise NotImplementedError("pyrecdp required pyspark pre-installed. Please do pip install pyspark==3.3.1")
 
-os.environ["JAVA_HOME"]="/usr/lib/jvm/java-8-openjdk-amd64/"
+if 'JAVA_HOME' not in os.environ:
+    os.environ["JAVA_HOME"]="/usr/lib/jvm/java-8-openjdk-amd64/"
+else:
+    print(f"JAVE_HOME is {os.environ['JAVA_HOME']}")
 os.environ["PYSPARK_PYTHON"]=sys.executable
 os.environ["PYSPARK_DRIVER_PYTHON"]=sys.executable
 os.environ["PYSPARK_WORKER_PYTHON"]=sys.executable
