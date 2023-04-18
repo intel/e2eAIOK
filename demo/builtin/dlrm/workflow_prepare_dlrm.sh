beeef603e6b1bda53da6c548d462dd1f1662ac13
Chendi.Xue <chendi.xue@intel.com>
2023-04-18 15:27:11 -0500

    [v1.1][ISSUE-218] DLRM demo update with fixes on infer error (#269)
    * update dlrm with workflow_prepare script
    
    Signed-off-by: Xue, Chendi <chendi.xue@intel.com>
    
    * add customized train script to dlrm sda
    
    Signed-off-by: Xue, Chendi <chendi.xue@intel.com>
    
    * update demo path
    
    Signed-off-by: Xue, Chendi <chendi.xue@intel.com>
    
    ---------
    
    Signed-off-by: Xue, Chendi <chendi.xue@intel.com>

--- demo/builtin/dlrm/workflow_prepare_dlrm.sh
+++ demo/builtin/dlrm/workflow_prepare_dlrm.sh
@@ -1,3 +1,4 @@
-git clone https://github.com/intel/e2eAIOK.git; cd e2eAIOK; git submodule update --init --recursive
+git clone https://github.com/intel/e2eAIOK.git;
 mv e2eAIOK e2eaiok
-cd e2eAIOK/modelzoo/dlrm; sh patch_dlrm.sh
+cd e2eaiok; git submodule update --init --recursive
+cd modelzoo/dlrm; sh patch_dlrm.sh
