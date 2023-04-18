066946c5b0491eb6ce0eff323fbb7574c6fff48c
Zheng Cai <zheng.cai@intel.com>
2023-04-18 14:48:46 +0800

    refine

--- demo/builtin/resnet/workflow_prepare_resnet.sh
+++ demo/builtin/resnet/workflow_prepare_resnet.sh
@@ -2,7 +2,7 @@
 git clone https://github.com/intel/e2eAIOK.git
 mv e2eAIOK e2eaiok
 cd e2eaiok
-git submodule update --init -recursive
+git submodule update --init --recursive
 
 # apply patch
 cd modelzoo/resnet && bash patch_resnet.sh
