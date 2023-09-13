cc9ef3c711b40e8a59c4e377662b3ffac94c2575
haojinIntel <hao.jin@intel.com>
2023-09-13 23:54:15 +0800

    Optimize the language_identify by adding the function to download model automatically

--- RecDP/pyrecdp/primitives/llmutils/utils.py
+++ RecDP/pyrecdp/primitives/llmutils/utils.py
@@ -1,16 +1,34 @@
 import os
 from multiprocessing import Pool, cpu_count
 from math import ceil
+import subprocess
 from tqdm import tqdm
 import ftfy
 import string
 import re
+import wget
+import urllib.error
+
 
 def get_data_files(data_dir):
     files = sorted(os.listdir(data_dir))
     files = list(filter(lambda file_: '.jsonl' in file_, files))
     files = [os.path.join(data_dir, i) for i in files]
     return files
+
+def get_target_file_list(data_dir, file_type):
+    os.system('pwd')
+    cmd = ["find", data_dir, "-name", f"*.{file_type}"]
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    exitcode = proc.returncode
+    if exitcode != 0:
+        return []
+    else:
+        ret = stdout.decode("utf-8").split('\n')[:-1]
+        ret = [i.replace(data_dir, "") for i in ret]
+        ret = [i[1:] if i[0] == '/' else i for i in ret]
+        return ret
 
 def get_nchunks_and_nproc(n_tasks, n_part = -1):
     n_proc = cpu_count()
@@ -43,3 +61,11 @@ def clean_str(s):
     s = s.lower().translate(str.maketrans("", "", string.punctuation))
     s = re.sub(r"\s+", " ", s.strip())
     return s
+
+def download_file(remote_path, target_path):
+        os.makedirs(os.path.dirname(target_path), exist_ok=True)
+        try:
+            wget.download(remote_path, out=target_path)
+        except urllib.error.HTTPError:
+            print("Failed to the file. Please check the url and network.")
+            exit(1)
--- RecDP/tests/test_llmutils.py
+++ RecDP/tests/test_llmutils.py
@@ -3,8 +3,6 @@ import sys
 import pandas as pd
 from pathlib import Path
 import os
-import wget
-import urllib.error
 pathlib = str(Path(__file__).parent.parent.resolve())
 print(pathlib)
 try:
@@ -16,6 +14,9 @@ except:
 from pyrecdp.primitives.llmutils import near_dedup, shrink_document_MP, text_to_jsonl_MP, pii_remove, \
     filter_by_blocklist, language_identify, Classifier
 
+from pyrecdp.primitives.llmutils.utils import get_target_file_list, download_file
+from pyrecdp.primitives.llmutils.language_identify import fasttext_model_url
+
 cur_dir = str(Path(__file__).parent.resolve())
 
 
@@ -25,7 +26,6 @@ class Test_LLMUtils(unittest.TestCase):
         self.data_dir = "tests/data/llm_data/"
         self.dup_dir = "./near_dedup/"
         self.fasttext_model = "./fasttext_model/lid.bin"
-        self.fasttext_mode_url = "https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.bin"
 
     def test_near_dedup(self):
         data_files = self.data_files
@@ -72,17 +72,12 @@ class Test_LLMUtils(unittest.TestCase):
         filter_by_blocklist(data_dir, out_dir)
 
     def test_language_identify(self):
-        data_files = self.data_files
-        data_dir = self.data_dir
+        data_dir = os.path.join(cur_dir, "data/llm_data")
+        data_files = get_target_file_list(data_dir, "jsonl")
         fasttext_model_dir = os.path.abspath(self.fasttext_model)
         if not os.path.exists(fasttext_model_dir):
-            os.makedirs(os.path.dirname(fasttext_model_dir), exist_ok=True)
-            try:
-                wget.download(self.fasttext_mode_url, out=fasttext_model_dir)
-            except urllib.error.HTTPError:
-                print("Failed to download DL language model. Please check your network.")
-                exit(1)
+            download_file(fasttext_model_url, fasttext_model_dir)
         model = Path(fasttext_model_dir)
         classifier = Classifier(model, 'text', 'lang')
         language_identify_output_dir = os.path.join(data_dir, "language_identify")
-        language_identify(data_files, classifier, language_identify_output_dir, enable_ray=False)
+        language_identify(data_dir, data_files, classifier, language_identify_output_dir, enable_ray=False)
