4559754c26fbf77c65f30418856aa8a4b46b1983
haojinIntel <hao.jin@intel.com>
2023-09-15 22:48:09 +0800

    Optimize the language_identify function. (#324)
    * Optimize the language_identify by adding the function to download model automatically
    
    * Small optimization for download_file funtion

--- RecDP/pyrecdp/primitives/llmutils/utils.py
+++ RecDP/pyrecdp/primitives/llmutils/utils.py
@@ -1,10 +1,15 @@
 import os
 from multiprocessing import Pool, cpu_count
 from math import ceil
+import subprocess
 from tqdm import tqdm
 import ftfy
 import string
 import re
+import wget
+import urllib.error
+
+
 from pyspark.sql.functions import input_file_name
 from pyspark.sql.types import StructType,StructField, StringType, IntegerType, ArrayType
 import pyspark.sql.functions as F
@@ -58,11 +63,26 @@ def global_unique_id(df, col_name):
     ret_df = ret_df.select(F.concat_ws("@", F.lit("global_id"), F.monotonically_increasing_id()).alias(col_name), "*")
     return ret_df
 
+
 def get_data_files(data_dir):
     files = sorted(os.listdir(data_dir))
     files = list(filter(lambda file_: '.jsonl' in file_, files))
     files = [os.path.join(data_dir, i) for i in files]
     return files
+
+def get_target_file_list(data_dir, file_type):
+    os.system('pwd')
+    cmd = ["find", data_dir, "-name", f"*.{file_type}"]
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    stdout, stderr = proc.communicate()
+    exitcode = proc.returncode
+    if exitcode != 0:
+        return []
+    else:
+        ret = stdout.decode("utf-8").split('\n')[:-1]
+        ret = [i.replace(data_dir, "") for i in ret]
+        ret = [i[1:] if i[0] == '/' else i for i in ret]
+        return ret
 
 def get_nchunks_and_nproc(n_tasks, n_part = -1):
     n_proc = cpu_count()
@@ -95,3 +115,11 @@ def clean_str(s):
     s = s.lower().translate(str.maketrans("", "", string.punctuation))
     s = re.sub(r"\s+", " ", s.strip())
     return s
+
+def download_file(remote_path, target_path):
+        os.makedirs(os.path.dirname(target_path), exist_ok=True)
+        try:
+            wget.download(remote_path, out=target_path)
+        except urllib.error.HTTPError as e:
+            print("Failed to download the file. Please check the url and network.")
+            raise e
