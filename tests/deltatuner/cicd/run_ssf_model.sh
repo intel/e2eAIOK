27894af92ca10e83d4320a4f739b81887a0bc450
zhouyu5 <yu.zhou@intel.com>
2023-10-09 08:21:18 +0000

    add

--- tests/deltatuner/cicd/run_ssf_model.sh
+++ tests/deltatuner/cicd/run_ssf_model.sh
@@ -13,9 +13,9 @@ mkdir -p $LOG_PATH $MODEL_SAVE_PATH
 
 # fine-tune with ssf
 # gpt2 EleutherAI/gpt-j-6b bigscience/bloom facebook/opt-125m EleutherAI/gpt-neo-125m tiiuae/falcon-7b
-# EleutherAI/gpt-j-6b bigscience/bloom tiiuae/falcon-7b
+# EleutherAI/gpt-j-6b bigscience/bloom-560m tiiuae/falcon-7b
 # mosaicml/mpt-7b
-model_name_list="mosaicml/mpt-7b"
+model_name_list="bigscience/bloom-560m"
 for model_name in $model_name_list
 do
     model_name_or_path=${model_name}
