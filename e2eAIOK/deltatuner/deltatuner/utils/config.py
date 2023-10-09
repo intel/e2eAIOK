e06ef20c8ee82959de9124f37158a58f51d2e4a0
Yu-Zhou <yu.zhou@intel.com>
2023-10-09 20:30:53 +0800

    Dtuner models (#7)
    * bug fix

--- e2eAIOK/deltatuner/deltatuner/utils/config.py
+++ e2eAIOK/deltatuner/deltatuner/utils/config.py
@@ -1,11 +1,13 @@
 import enum
 from dataclasses import  field
 from typing import List, Optional, Tuple, Union
+from peft.utils import TRANSFORMERS_MODELS_TO_LORA_TARGET_MODULES_MAPPING
 
 class DeltaTunerType(str, enum.Enum):
     SSF = "SSF"
 
-TRANSFORMERS_MODELS_TO_SSF_TARGET_MODULES_MAPPING = {
+TRANSFORMERS_MODELS_TO_SSF_TARGET_MODULES_MAPPING = TRANSFORMERS_MODELS_TO_LORA_TARGET_MODULES_MAPPING
+TRANSFORMERS_MODELS_TO_SSF_TARGET_MODULES_MAPPING.update({
     "llama": ["q_proj", "v_proj"],
     "mpt": ["Wqkv","out_proj","up_proj","down_proj"]
-}
+})
---
+++ tests/deltatuner/cicd/run_ssf_model.sh
@@ -0,0 +1,49 @@
+set -x
+
+cd /home/vmagent/app/e2eaiok/e2eAIOK/deltatuner
+pip install -e .
+
+cd /home/vmagent/app/e2eaiok
+
+DATA_PATH="/home/vmagent/app/data"
+LOG_PATH=$DATA_PATH"/dtuner_test/log"
+MODEL_SAVE_PATH=$DATA_PATH"/dtuner_test/models"
+
+mkdir -p $LOG_PATH $MODEL_SAVE_PATH
+
+# fine-tune with ssf
+# gpt2 EleutherAI/gpt-j-6b bigscience/bloom-560m facebook/opt-125m EleutherAI/gpt-neo-125m tiiuae/falcon-7b
+# EleutherAI/gpt-j-6b tiiuae/falcon-7b
+# mosaicml/mpt-7b
+model_name_list="tiiuae/falcon-7b"
+for model_name in $model_name_list
+do
+    model_name_or_path=${model_name}
+    short_model_name=`echo $model_name | cut -d/ -f2`
+    model_save_path=${MODEL_SAVE_PATH}"/"${short_model_name}"_ssf"
+    log_save_path=$LOG_PATH"/"${short_model_name}"_ssf-1epoch.log"
+    python example/instruction_tuning_pipeline/finetune_clm.py \
+        --model_name_or_path $model_name_or_path \
+        --train_file "$DATA_PATH/alpaca_data.json" \
+        --dataset_concatenation \
+        --per_device_train_batch_size 8 \
+        --per_device_eval_batch_size 8 \
+        --gradient_accumulation_steps 1 \
+        --validation_split_percentage 30 \
+        --do_train \
+        --do_eval \
+        --learning_rate 1e-4 \
+        --num_train_epochs 1 \
+        --logging_steps 100 \
+        --save_total_limit 1 \
+        --log_level info \
+        --save_strategy epoch \
+        --trust_remote_code True \
+        --output_dir $model_save_path \
+        --delta ssf \
+        --denas False \
+        --debugs \
+        --no_cuda \
+        2>&1 | tee $log_save_path
+    # rm -rf ~/.cache/huggingface/hub
+done
