ed478985f28164b37f7666b0d14c7ef4cf7925c4
zhouyu5 <yu.zhou@intel.com>
2023-10-09 19:44:39 +0000

    update test scripts

--- tests/deltatuner/finetune/run_lora.sh
+++ tests/deltatuner/finetune/run_lora.sh
@@ -1,56 +1,49 @@
 #!/bin/bash
 set -x
-mkdir -p log models
 
-# fine-tune mpt-7b with lora
-python example/instruction_tuning_pipeline/finetune_clm.py \
-    --model_name_or_path "/home/vmagent/app/dataset/mpt-7b" \
-    --train_file "/home/vmagent/app/dataset/stanford_alpaca/alpaca_data.json" \
-    --dataset_concatenation \
-    --per_device_train_batch_size 8 \
-    --per_device_eval_batch_size 8 \
-    --gradient_accumulation_steps 1 \
-    --do_train \
-    --do_eval \
-    --validation_split_percentage 30 \
-    --learning_rate 1e-4 \
-    --num_train_epochs 1 \
-    --logging_steps 100 \
-    --save_total_limit 1 \
-    --log_level info \
-    --save_strategy epoch \
-    --output_dir models/mpt_lora_model \
-    --peft lora \
-    --denas False \
-    --trust_remote_code True \
-    --no_cuda \
-    --bf16 True 2>&1 | tee log/mpt-lora-run-1epoch.log
+DATA_PATH="/home/vmagent/app/data"
+LOG_PATH=$DATA_PATH"/dtuner_test/log"
+MODEL_SAVE_PATH=$DATA_PATH"/dtuner_test/models"
 
-# fine-tune llama2-7b with lora
-python example/instruction_tuning_pipeline/finetune_clm.py \
-    --model_name_or_path "/home/vmagent/app/dataset/Llama-2-7b-hf" \
-    --train_file "/home/vmagent/app/dataset/stanford_alpaca/alpaca_data.json" \
-    --dataset_concatenation \
-    --per_device_train_batch_size 8 \
-    --per_device_eval_batch_size 8 \
-    --gradient_accumulation_steps 1 \
-    --do_train \
-    --do_eval \
-    --validation_split_percentage 30 \
-    --learning_rate 1e-4 \
-    --num_train_epochs 1 \
-    --logging_steps 100 \
-    --save_total_limit 1 \
-    --log_level info \
-    --save_strategy epoch \
-    --output_dir models/llama2_lora_model \
-    --peft lora \
-    --denas False \
-    --trust_remote_code True \
-    --no_cuda \
-    --bf16 True 2>&1 | tee log/llama2-lora-run-1epoch.log
+mkdir -p $LOG_PATH $MODEL_SAVE_PATH
 
 
-# used for assign target modules to lora
-# --lora_target_modules q_proj v_proj k_proj o_proj up_proj down_proj \
-# --lora_target_modules Wqkv out_proj up_proj down_proj \
+# fine-tune with lora
+model_name_list="mpt-7b Llama-2-7b-hf"
+for model_name in $model_name_list
+do
+    model_name_or_path=${DATA_PATH}"/"${model_name}
+    model_save_path=${MODEL_SAVE_PATH}"/"${model_name}"-lora"
+    log_save_path=$LOG_PATH"/"${model_name}"-lora-1epoch.log"
+    python example/instruction_tuning_pipeline/finetune_clm.py \
+        --model_name_or_path $model_name_or_path \
+        --train_file $DATA_PATH"/alpaca_data.json" \
+        --dataset_concatenation \
+        --per_device_train_batch_size 8 \
+        --per_device_eval_batch_size 8 \
+        --gradient_accumulation_steps 1 \
+        --do_train \
+        --do_eval \
+        --validation_split_percentage 30 \
+        --learning_rate 1e-4 \
+        --num_train_epochs 1 \
+        --logging_steps 100 \
+        --save_total_limit 1 \
+        --log_level info \
+        --save_strategy epoch \
+        --output_dir $model_save_path \
+        --peft lora \
+        --denas False \
+        --trust_remote_code True \
+        --no_cuda \
+        2>&1 | tee $log_save_path
+done
+
+
+########################## advanced options ##########################
+# note 0: used for assign target modules
+    # llama: --lora_target_modules q_proj v_proj k_proj o_proj up_proj down_proj \
+    # mpt: --lora_target_modules Wqkv out_proj up_proj down_proj \
+# note 1: used for 4th Xeon or later (SPR etc.): --bf16 --no_cuda
+# note 2: used for 3th Xeon or before (ICX etc.): --no_cuda
+# note 3: used for GPU server: --fp16
