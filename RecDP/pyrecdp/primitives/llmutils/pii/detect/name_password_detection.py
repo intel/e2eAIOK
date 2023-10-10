308fa7a723243f4b9895c642d2214bca20cb3a81
ChaoJun Zhang <chaojun.zhang@intel.com>
2023-10-11 00:38:27 +0800

    [v1.2][ISSUE-306] PII Remove support remove entity types specified by the user. (#371)
    * pii remove operator support remove types
    
    * Update text_pii_remove.py by adding entity_types to settings
    
    ---------
    
    Co-authored-by: Chendi.Xue <chendi.xue@intel.com>

--- RecDP/pyrecdp/primitives/llmutils/pii/detect/name_password_detection.py
+++ RecDP/pyrecdp/primitives/llmutils/pii/detect/name_password_detection.py
@@ -1,21 +1,25 @@
 from transformers import Pipeline
+from .utils import PIIEntityType
 
 
-def detect_name_password(content: str, pipeline: Pipeline):
+def detect_name_password(content: str, pipeline: Pipeline, entity_types=None):
     """Detects name and password in a string using bigcode/starpii model
     Args:
-      pipe: a transformer model
+      entity_types: detection types
+      pipeline: a transformer model
       content (str): A string containing the text to be analyzed.
-      sentences ([str]): A string array
     Returns:
         A list of dicts containing the tag type, the matched string, and the start and
         end indices of the match.
     """
+    if entity_types is None:
+        entity_types = [PIIEntityType.NAME, PIIEntityType.PASSWORD]
     matches = []
     try:
         for entity in pipeline(content):
             entity_group = entity["entity_group"]
-            if "NAME" == entity_group or "PASSWORD" == entity_group:
+            if ("NAME" == entity_group and PIIEntityType.NAME in entity_types) or \
+                    ("PASSWORD" == entity_group and PIIEntityType.PASSWORD in entity_types):
                 matches.append(
                     {
                         "tag": entity_group,
