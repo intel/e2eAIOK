1db7f506ba531043b69415ede287bc65655cc263
haojinIntel <hao.jin@intel.com>
2023-10-25 12:04:51 +0800

    Add parameter explanation for ops

--- RecDP/pyrecdp/primitives/llmutils/toxicity_score.py
+++ RecDP/pyrecdp/primitives/llmutils/toxicity_score.py
@@ -3,6 +3,14 @@ from pyrecdp.core.utils import Timer
 
 
 def toxicity_score_spark(spark_df, text_key='text', threshold=0, model_type="multilingual", huggingface_config_path=None):
+    """
+    Use specific toxicity_score to predict document oxicity scores on your dataset
+    :param df_spark: spark dataframe
+    :param text_key: the field key name to be applied toxicity_score operation. It's "text" in default
+    :param model_type: we can use one of ["multilingual", "unbiased", "original"] type of detoxify lib.
+    :param huggingface_config_path: the local model config for detoxify model.
+    :return:
+    """
     from pyrecdp.primitives.operations import TextToxicity
     op = TextToxicity(text_key=text_key, threshold=threshold, model_type=model_type, huggingface_config_path=huggingface_config_path)
     toxicity_df = op.process_spark(spark_df.sparkSession, spark_df)
@@ -16,7 +24,16 @@ def toxicity_score(dataset_path,
                    threshold=0,
                    model_type="multilingual",
                    huggingface_config_path=None):
-
+    """
+    Use specific toxicity_score to predict document oxicity scores on your dataset
+    :param dataset_path: the path of dataset folder
+    :param result_path: the path of result folder
+    :param text_key: the field key name to be applied toxicity_score operation. It's "text" in default
+    :param threshold: the threshold of toxicity score which will determine the data kept or not. the value range is [0, 1)
+    :param model_type: we can use one of ["multilingual", "unbiased", "original"] type of detoxify lib.
+    :param huggingface_config_path: the local model config for detoxify model.
+    :return:
+    """
     from pyrecdp.LLM import TextPipeline
     from pyrecdp.primitives.operations import JsonlReader, ParquetReader, TextToxicity, ParquetWriter
 
