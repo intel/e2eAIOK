31fe68626a202471a3a05b3aac2118fa21093b4a
Xue, Chendi <chendi.xue@intel.com>
2023-10-26 02:44:20 +0000

    update test
    Signed-off-by: Xue, Chendi <chendi.xue@intel.com>

--- RecDP/tests/test_llmutils_pipelines.py
+++ RecDP/tests/test_llmutils_pipelines.py
@@ -34,7 +34,7 @@ class Test_LLMUtils_Pipeline(unittest.TestCase):
             for dir_name in dir_name_list:
                 print(dir_name)
                 display(pd.read_parquet(os.path.join("ResumableTextPipeline_output", dir_name)).head())
-            #shutil.rmtree("ResumableTextPipeline_output")
+            shutil.rmtree("ResumableTextPipeline_output")
         except Exception as e:
             print(e)
         return super().tearDown()
