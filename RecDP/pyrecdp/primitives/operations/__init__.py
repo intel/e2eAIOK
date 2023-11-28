6f641a6adb52d510fc91ed15441dfdb06dbd08d7
ChaoJun Zhang <chaojun.zhang@intel.com>
2023-11-28 23:09:45 +0800

    [v1.2][ISSUE-306] Add ParagraphsTextSplitter (#450)
    * Add Paragraphs TextSplitter
    
    * add markdownify and bs4 automated
    
    * use html.parser instead of lxml for less dependencies

--- RecDP/pyrecdp/primitives/operations/__init__.py
+++ RecDP/pyrecdp/primitives/operations/__init__.py
@@ -1,4 +1,5 @@
 from .base import Operation, BaseOperation
+
 try:
     from .dataframe import RDDToDataFrameConverter, SparkDataFrameToDataFrameConverter
     from .data import DataFrameOperation, DataLoader
@@ -12,22 +13,49 @@ try:
     from .type import TypeInferOperation
     from .tuple import TupleOperation
     from .custom import CustomOperation
-    from .encode import OnehotEncodeOperation, ListOnehotEncodeOperation, TargetEncodeOperation, \
+    from .encode import (
+        OnehotEncodeOperation,
+        ListOnehotEncodeOperation,
+        TargetEncodeOperation,
         CountEncodeOperation
+    )
     from pyrecdp.primitives.estimators.lightgbm import LightGBM
 except:
     pass
 try:
-    from .text_reader import DatasetReader, JsonlReader, ParquetReader, SourcedJsonlReader, SourcedParquetReader, PerfileSourcedJsonlReader, PerfileSourcedParquetReader, GlobalParquetReader, GlobalJsonlReader
-    from .text_writer import PerfileParquetWriter, ParquetWriter, JsonlWriter, ClassifyJsonlWriter, ClassifyParquetWriter
+    from .text_reader import (
+        DatasetReader,
+        JsonlReader,
+        ParquetReader,
+        SourcedJsonlReader,
+        SourcedParquetReader,
+        PerfileSourcedJsonlReader,
+        PerfileSourcedParquetReader,
+        GlobalParquetReader,
+        GlobalJsonlReader
+    )
+    from .text_writer import (
+        PerfileParquetWriter,
+        ParquetWriter,
+        JsonlWriter,
+        ClassifyJsonlWriter,
+        ClassifyParquetWriter
+    )
     from .text_normalize import TextNormalize
     from .text_bytesize import TextBytesize
     from .filter import *
     from .text_fixer import TextFix
     from .text_language_identify import LanguageIdentify
-    from .text_split import DocumentSplit
+    from .text_split import DocumentSplit, ParagraphsTextSplitter
     from .text_pii_remove import PIIRemoval
-    from .text_deduplication import FuzzyDeduplicate, GlobalDeduplicate, FuzzyDeduplicateGenDict, FuzzyDeduplicateApplyDict, GlobalDeduplicateGenDict, GlobalDeduplicateApplyDict
+    from .text_deduplication import (
+        FuzzyDeduplicate,
+        GlobalDeduplicate,
+        FuzzyDeduplicateGenDict,
+        FuzzyDeduplicateApplyDict,
+        GlobalDeduplicateGenDict,
+        GlobalDeduplicateApplyDict
+    )
     from .text_qualityscorer import TextQualityScorer
     from .text_diversityindicate import TextDiversityIndicate
     from .text_custom import TextCustomerMap, TextCustomerFilter
