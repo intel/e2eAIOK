a6f9666052ead4770ef89d37325acdf240c2724c
zhouyu5 <yu.zhou@intel.com>
2023-12-04 21:01:29 +0000

    add a new package name

--- e2eAIOK/deltatuner/setup.py
+++ e2eAIOK/deltatuner/setup.py
@@ -12,46 +12,49 @@ REQUIRED_PACKAGES = [
     'sigopt', 'torchsummary'
 ]
 
-setuptools.setup(
-    name="deltatuner",
-    version=VERSION,
-    author="Intel AIA",
-    author_email="bdf.aiok@intel.com",
-    description="Intel extension for peft with PyTorch and DENAS",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/intel/e2eAIOK/",
-    download_url='https://github.com/intel/e2eAIOK/',
-    packages=setuptools.find_packages(
-        exclude=["example", "docker", ]),
-    package_data={'deltatuner': ['version', '*/*/*', '*/*/*/*'], }, 
-    python_requires=">=3.7",  # '>=3.4',  # !=3.4.*
-    install_requires=REQUIRED_PACKAGES,
-    extras_require={
+name_list = ["deltatuner", "e2eAIOK-deltatuner"]
 
-    },
-    entry_points={
-    },
-    classifiers=(
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Science/Research',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ),
-    license="Apache-2.0",
-    keywords=[
-        'deep learning', 'LLM', 'fine-tuning', 'pytorch', 'peft',
-        'lora', 'NAS'
-    ],
-)
+for name in name_list:
+    setuptools.setup(
+        name=name,
+        version=VERSION,
+        author="Intel AIA",
+        author_email="bdf.aiok@intel.com",
+        description="Intel extension for peft with PyTorch and DENAS",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/intel/e2eAIOK/",
+        download_url='https://github.com/intel/e2eAIOK/',
+        packages=setuptools.find_packages(
+            exclude=["example", "docker", ]),
+        package_data={'deltatuner': ['version', '*/*/*', '*/*/*/*'], }, 
+        python_requires=">=3.7",  # '>=3.4',  # !=3.4.*
+        install_requires=REQUIRED_PACKAGES,
+        extras_require={
+
+        },
+        entry_points={
+        },
+        classifiers=(
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: OS Independent",
+            'Intended Audience :: Developers',
+            'Intended Audience :: Education',
+            'Intended Audience :: Science/Research',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
+            'Topic :: Scientific/Engineering',
+            'Topic :: Scientific/Engineering :: Artificial Intelligence',
+            'Topic :: Software Development',
+            'Topic :: Software Development :: Libraries',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+        ),
+        license="Apache-2.0",
+        keywords=[
+            'deep learning', 'LLM', 'fine-tuning', 'pytorch', 'peft',
+            'lora', 'NAS'
+        ],
+    )
