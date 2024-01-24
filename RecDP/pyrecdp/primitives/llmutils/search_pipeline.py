858fda08dabbfbd26f4b073ecbab7c16fc1c71c0
Yao, Qing <qing.yao@intel.com>
2024-01-24 17:49:18 +0800

    Support DocumentIngestion return in_memory db handler.

--- RecDP/pyrecdp/primitives/llmutils/search_pipeline.py
+++ RecDP/pyrecdp/primitives/llmutils/search_pipeline.py
@@ -1,19 +1,18 @@
+from faiss import IndexFlatL2
+from langchain_community.docstore import InMemoryDocstore
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.utilities.google_search import GoogleSearchAPIWrapper
 from langchain_community.vectorstores.faiss import FAISS
 from langchain_core.tools import Tool
 
 from pyrecdp.LLM import TextPipeline
-from pyrecdp.primitives.operations import UrlLoader, RAGTextFix, DocumentSplit, LengthFilter, GoogleSearchTool
+from pyrecdp.primitives.operations import UrlLoader, RAGTextFix, DocumentSplit, LengthFilter, GoogleSearchTool, \
+    DocumentIngestion
 
 
-def content_similarity_search(query, texts, k=4,
-                              embedding_model_name='sentence-transformers/all-MiniLM-L6-v2'):
-    embeddings = HuggingFaceEmbeddings(model_name=embedding_model_name)
-    db = FAISS.from_texts(texts, embeddings)
+def db_similarity_search(query, db, k=4):
     docs = db.similarity_search(query=query, k=k)
     return docs
-
 
 def get_search_results(query):
     search = GoogleSearchTool(query=query)
@@ -25,7 +24,7 @@ def get_search_results(query):
         "chunk_overlap": 0,
         "separators": ["\n\n", "\n", " ", ""],
     }
-
+    embedding_model_name = 'sentence-transformers/all-MiniLM-L6-v2'
     pipeline = TextPipeline()
     ops = [search]
 
@@ -34,20 +33,25 @@ def get_search_results(query):
             UrlLoader(text_key='url', text_to_markdown=False),
             RAGTextFix(re_sentence=True),
             DocumentSplit(text_splitter=text_splitter, text_splitter_args=text_splitter_args),
-            LengthFilter()
+            LengthFilter(),
+            DocumentIngestion(
+                vector_store='FAISS',
+                vector_store_args={"in_memory": True, "index": 'search'},
+                embeddings='HuggingFaceEmbeddings',
+                embeddings_args={"model_name": embedding_model_name},
+                return_db_handler=True
+            )
         ]
     )
     pipeline.add_operations(ops)
-    ds = pipeline.execute()
-    texts = [row["text"] for row in ds.iter_rows()]
-
-    return texts
+    db = pipeline.execute()
+    return db
 
 
 if __name__ == '__main__':
     query = "chatgpt latest version?"
-    texts = get_search_results(query)
-    res = content_similarity_search(query, texts)
+    db = get_search_results(query)
+    res = db_similarity_search(query, db)
     for line in res:
         print(line)
         print("_" * 40)
