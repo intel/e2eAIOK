28e8b80e4c0a286b871c77aef5fcff944293dbee
Chendi.Xue <chendi.xue@intel.com>
2024-01-29 17:35:53 -0600

    Update setup.py

--- RecDP/setup.py
+++ RecDP/setup.py
@@ -41,7 +41,7 @@ class SetupSpec:
             "cloudpickle",
             "wget==3.2",
             "pyspark==3.4.0",
-            "ray>=2.7.1",
+            "ray==2.7.1",
             "matplotlib",
             "jsonlines==3.1.0",
             "regex==2023.6.3",
